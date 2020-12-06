7a05b605ce239560fea6bc8a8175615bf71513f7
Alexander <alex.kononok@gmail.com>
2020-12-06 20:22:52 +0300

    Add: button for add image

--- SafeGallery/ViewController.swift
+++ SafeGallery/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  private let pickerController = UIImagePickerController()
+  
   private var loginTextField: UITextField?
   private var passwordTextField: UITextField?
   
@@ -24,14 +26,26 @@ class ViewController: UIViewController {
   override func viewDidAppear(_ animated: Bool) {
     super.viewDidAppear(animated)
     
+    // MARK: - Add button on the bottom main view
+    let heightButton: CGFloat = 100
+    let addImageButton = UIButton(frame: CGRect(x: view.frame.minX, y: view.frame.maxY - heightButton, width: view.frame.width, height: heightButton))
+    addImageButton.backgroundColor = .cyan
+    addImageButton.setTitleColor(.black, for: .normal)
+    addImageButton.setTitle("Add image", for: .normal)
+    addImageButton.addTarget(self, action: #selector(addImageButtonPressed), for: .touchUpInside)
+    
+    // MARK: - Add title for navigation controller
+    self.title = "Gallery"
+    
+    // MARK: - Add Sign In alert
     let signInAlert = UIAlertController(title: "Sign in", message: nil, preferredStyle: .alert)
     let warningAlert = UIAlertController(title: "WARNING", message: "Your login or password is wrong.\nTry again.", preferredStyle: .alert)
     let okWarningAction = UIAlertAction(title: "OK", style: .default) { (_) in
       self.present(signInAlert, animated: true)
     }
     let okSignInAction = UIAlertAction(title: "OK", style: .default) { (_) in
-      if (self.loginTextField?.text == self.loginAdmin) ?? (self.passwordTextField?.text == self.passwordAdmin) {
-        
+      if (self.loginTextField?.text == self.loginAdmin) && (self.passwordTextField?.text == self.passwordAdmin) {
+        self.view.addSubview(addImageButton)
       } else {
         self.present(warningAlert, animated: true) {
           self.present(signInAlert, animated: true)
@@ -55,5 +69,10 @@ class ViewController: UIViewController {
     present(signInAlert, animated: true)
   }
 
+  // MARK: - Open picker with images
+  @objc func addImageButtonPressed(sender: UIButton!) {
+    present(pickerController, animated: true)
+  }
+  
 }
 
